c6b0d9f9eab3d60c20390fb05c82afdee22ae5f4
Tim Ryan <tim@timryan.dev>
2020-10-25 14:13:06 -0400

    fixed client nginx default.conf

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React Now
           </a>
           <Link to="/">Homepage</Link>
           <Link to="/otherpage">Other Page</Link>
